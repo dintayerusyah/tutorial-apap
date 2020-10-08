b0f8b6a74d1066e95673884fe291200cf4103e5a
dintayerusyah <dintasimatupang@gmail.com>
2020-10-08 17:37:58 +0700

    Create Model

--- haidokter/src/main/java/apap/tutorial/haidokter/model/ResepModel.java
+++ haidokter/src/main/java/apap/tutorial/haidokter/model/ResepModel.java
@@ -1,23 +1,48 @@
 package apap.tutorial.haidokter.model;
 
-public class ResepModel {
-    private String noResep;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "resep")
+public class ResepModel implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long noResep;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "nama_dokter", nullable = false)
     private String namaDokter;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "nama_pasien", nullable = false)
     private String namaPasien;
+
+    @NotNull
+    @Size(max = 40)
+    @Column(name = "catatan", nullable = false)
     private String catatan;
 
-    public ResepModel(String noResep, String namaDokter, String namaPasien, String catatan) {
-        this.noResep = noResep;
-        this.namaDokter = namaDokter;
-        this.namaPasien = namaPasien;
-        this.catatan = catatan;
-    }
+    @OneToMany(mappedBy = "resepModel", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<ObatModel> listObat;
 
-    public String getNoResep() {
+    // public ResepModel(Long noResep, String namaDokter, String namaPasien, String catatan) {
+    //     this.noResep = noResep;
+    //     this.namaDokter = namaDokter;
+    //     this.namaPasien = namaPasien;
+    //     this.catatan = catatan;
+    // }
+
+    public Long getNoResep() {
         return this.noResep;
     }
 
-    public void setNoResep(String noResep) {
+    public void setNoResep(Long noResep) {
         this.noResep = noResep;
     }
 
